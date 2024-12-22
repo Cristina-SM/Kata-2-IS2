5a27ebbe968dd2bc816e9f9eead8bec4bb879a81
Cristina-SM <cristinasantanamartindevelop@gmail.com>
2024-12-22 12:39:30 +0000

    Refactor

--- src/main/java/software/ulpgc/kata2/Title.java
+++ src/main/java/software/ulpgc/kata2/Title.java
@@ -1,4 +1,4 @@
-package software.ulpgc;
+package software.ulpgc.kata2;
 
 public class Title {
     public final String id;
--- src/main/java/software/ulpgc/kata2/TitleDeserializer.java
+++ src/main/java/software/ulpgc/kata2/TitleDeserializer.java
@@ -1,4 +1,4 @@
-package software.ulpgc;
+package software.ulpgc.kata2;
 
 public interface TitleDeserializer {
     Title deserializer(String line);
